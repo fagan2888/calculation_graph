d9a6842d461c1fe43b16b880f7cc893faaaef572
richard <richard.s.shepherd@gmail.com>
2015-02-28 20:24:54 +0000

    calculation_graph

--- graph_node.py
+++ graph_node.py
@@ -3,22 +3,39 @@ class GraphNode(object):
     """
     See GraphManager for a description of the working of the graph.
 
-    Methods to implement in derived classes
-    ---------------------------------------
-    This is a base class for nodes in the calculation graph. The main methods
-    that you implement in your derived nodes are:
+    This is a base class for nodes in the calculation graph. The key methods you
+    can implement in your derived node class are:
 
-    - set_dependencies(): You choose parent nodes whose data and calculations
-                          your node depends on here.
+    set_dependencies() [optional, though implemented in most nodes]
+    ------------------
+    You choose parent nodes whose data and calculations your node depends on here.
 
-    - calculate(): You perform your calculations here.
+    When a node is first created, set_dependencies() is always called before calculate().
 
-    - dispose(): Called when a node is no longer needed in the graph. You should
-                 clean up any non-node resources here. Note that links to parent
-                 nodes will be automatically removed, so you do not need to do this
-                 yourself.
+    All nodes that depend on parent nodes must implement this method. You can omit
+    it for 'leaf' nodes which do not depend on any other nodes.
 
+    calculate()
+    -----------
+    You perform your calculations here.
 
+    Return GraphNode.CALCULATE_CHILDREN to carry on calculating further nodes in the
+    graph, GraphNode.DO_NOT_CALCULATE_CHILDREN not to, for example if no output data
+    has changed as a result of the calculation.
+
+    Note: If you return DO_NOT_CALCULATE_CHILDREN, child nodes will still be calculated
+          if other parent nodes have caused them to do so.
+
+    dispose() [optional]
+    ---------
+    Called when a node is no longer needed in the graph. You should clean up any non-node
+    resources here. Note that links to parent nodes will be automatically removed, so you
+    do not need to do this yourself. If you have no non-node resources, you can omit this
+    method from your node class.
+
+    make_id(identity_param1, identity_param2, ...) [optional]
+    ----------------------------------------------
+    Nodes have an internal unique ID made up from their
 
     Quality
     -------
